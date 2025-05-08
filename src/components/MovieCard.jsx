52f16ef7ea0292896c58b20bea14db83c2ccc994
Harsh_Chouhan <harshcoding2004@email.com>
2025-05-08 21:36:03 +0530

    updated MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,20 +1,13 @@
 import React from "react";
 import { IMG_CDN_URL } from "../utils/constants";
 
-const MovieCard = ({ posterPath, title, className }) => {
-  const fallback = "https://via.placeholder.com/150x225?text=No+Image";
-  const imgSrc = posterPath
-    ? `https://image.tmdb.org/t/p/w300${posterPath}`
-    : fallback;
-
+const MovieCard = ({ posterPath }) => {
+  if (!posterPath) return null;
   return (
-    <img
-      src={imgSrc}
-      alt={title || "Movie Poster"}
-      className={`w-40 h-60 object-cover ${className}`}
-    />
+    <div className="w-32 md:w-40 lg:w-48 rounded-md overflow-hidden">
+      <img src={IMG_CDN_URL + posterPath} alt="Movie Poster" />
+    </div>
   );
 };
-
 
 export default MovieCard;
